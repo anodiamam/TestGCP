2042c661ad39774137bae3ff9e8242f0d30f0573
Debashish Nath <anodiam.am@gmail.com>
2024-07-20 19:27:56 +0530

    new version of GCP checked in

--- react101/src/App.js
+++ react101/src/App.js
@@ -4,18 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome To React JS
-        </p>
+      <header className="App-header" >
+        <img src={logo} className="App-logo" alt="logo"  style={{backgroundColor:'yellow'}} />
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Deploying application from Google Cloud Platform
         </a>
       </header>
     </div>
